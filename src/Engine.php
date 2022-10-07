ae04eaa49454ecd9c9d32a5dc531ce1fbee2411f
Denny <denny.cnf@gmail.com>
2022-10-07 13:20:39 +0600

    add rules in gitignore

--- src/Engine.php
+++ src/Engine.php
@@ -12,7 +12,7 @@ use function cli\prompt;
 function Hello()
 {
     global $name;
-    line('Welcome to the Brain Game');
+    line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
     return $name;
