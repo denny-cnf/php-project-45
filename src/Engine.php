b324deb3e8defe0b86a8c038df596cd79f6b99f1
Denny <denny.cnf@gmail.com>
2022-11-08 15:25:05 +0600

    fix bug & rem composer.lock

--- src/Engine.php
+++ src/Engine.php
@@ -10,7 +10,7 @@ const ROUNDS = 3;
 use function cli\line;
 use function cli\prompt;
 
-function hello($name)
+function hello()
 {
     line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
